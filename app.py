b2bec1cd2a6cb31adcd91f2bc78689b68fde6681
Emily Huang <ehuan015@gmail.com>
2025-05-15 16:57:16 -0700

    Add port binding for render deployment

--- app.py
+++ app.py
@@ -69,6 +69,10 @@ default_location = get_coord("121 Spear Street", "USA")
 def index():
     return render_template('index.html')
 
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
+
 # VISUALIZATION FUNCTIONS 
 @app.route('/visualize')
 def visualize():
