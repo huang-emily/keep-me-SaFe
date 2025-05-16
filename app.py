57e7ef73fdc65c7c6e3a299ae878d7d5d08a2df4
Emily Huang <ehuan015@gmail.com>
2025-05-15 17:11:23 -0700

    Change port value for port binding

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ def index():
 
 if __name__ == "__main__":
     from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+    serve(app, host="0.0.0.0", port=10000)
 
 # VISUALIZATION FUNCTIONS 
 @app.route('/visualize')
